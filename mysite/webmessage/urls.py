82d23f7e4d618c60da251d803501acdffc3b1522
sartorj <sartorj@uga.edu>
2016-03-03 12:33:06 -0500

    update

--- mysite/webmessage/urls.py
+++ mysite/webmessage/urls.py
@@ -4,10 +4,17 @@ from . import views
 
 
 urlpatterns = [
-    # ex: /polls/
+    # ex: /webmessage/
     url(r'^$', views.index, name='index'),
-    # ex: /polls/5/
-    url(r'^mailbox/$', views.detail, name='detail'),
+    # ex: /webmessage/mailbox/
+    url(r'^mailbox/$', views.mailbox, name='mailbox'),
+    # ex: /webmessage/mailbox/<username>
+    url(r'^mailbox/(?P<user_name>[^/]+)/$', views.detail, name='detail'),
     # ex: /polls/5/vote/
     url(r'^(?P<receiving_user>[0-9]+)/sending/$', views.sending, name='sending'),
+    # ex: /webmessage/users/
+    url(r'^users/$', views.users, name='users'),
+    # ex: /webmessage/register/
+    url(r'^register/$', views.register, name = 'register'),
+
 ]
--- mysite/webmessage/views.py
+++ mysite/webmessage/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.contrib.auth.models import User
+from django.shortcuts import render
+
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("You are at the webmessage index")
+    #return HttpResponse("You are at the webmessage index")
+    return render(request, 'webmessage/index.html')
 
-def detail(request):
-    return HttpResponse("You're looking at the mailbox of %s." % request.user)
+def mailbox(index):
+    return HttpResponse("You are at the mailbox index")
+
+def detail(request,user_name):
+
+    return HttpResponse("You're looking at the mailbox of %s." % user_name)
 
 def sending(request, receiving_user):
     return HttpResponse("You're sending a message to:  %s." % receiving_user)
+
+
+def users(request):
+    recent_user_list = User.objects.order_by('-username')
+    template = loader.get_template('webmessage/userindex.html')
+    context = {
+        'recent_user_list' : recent_user_list,
+    }
+    
+    return HttpResponse(template.render(context,request))
+
+
+def register(request):
+    context = {}
+    return render(request, 'webmessage/register.html')
